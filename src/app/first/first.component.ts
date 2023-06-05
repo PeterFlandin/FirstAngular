38ee63ce128cad2a5f5eacb0642dfdc6be1a4993
PeterFlandin <peterflandin@gmail.com>
2023-06-05 12:34:52 +0200

    first commit

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent {
+
+prenom : String = "je mappelle JeanJean";
+
+color = 'yellow';
+
+
+traitement() {
+  this.color = 'green';
+}
+
+
+}
